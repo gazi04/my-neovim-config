7d1e982cca0017c72d6364bc46add3ad681d6474
gazi04 <gazmendhalili2016@gmail.com>
2026-01-27 14:43:03 +0100

    :sparkles: Setting up intelephense for shopware

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,57 +1,44 @@
+local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
+local nvlsp = require("nvchad.configs.lspconfig")
+
 -- 1. Load Defaults
-require("nvchad.configs.lspconfig").defaults()
+nvlsp.defaults()
 
--- 2. Define Variables
-local configs = require "lspconfig.configs"
-local nvlsp = require "nvchad.configs.lspconfig"
-local util = require "lspconfig.util" -- THIS WAS MISSING
-local lspconfig = require "lspconfig"
+-- 2. Define the list of servers
+local servers = { "pyright", "ts_ls", "tailwindcss", "html", "cssls", "twiggy_language_server", "emmet_ls" }
 
--- 3. Standard Servers
-local servers = { "pyright", "intelephense", "laravel_ls", "ts_ls", "tailwindcss", "vue_ls", "html", "cssls", "twiggy_language_server", "emmet_ls" }
-vim.lsp.enable(servers)
-
--- Helper function to fix the "Unknown Scheme" by adding file:// prefix
-local function fix_uri(uri)
-  if uri and not string.match(uri, "^%w+://") then
-    return "file://" .. uri
-  end
-  return uri
-end
-
--- 1. Register the Shopware LSP
-if not configs.shopware_lsp then
-  configs.shopware_lsp = {
-    default_config = {
-      cmd = { "/home/gazi/go/bin/shopware-lsp" },
-      filetypes = { "php", "twig", "xml", "yaml" },
-      root_dir = util.root_pattern("bin/console", "composer.json", ".git"),
-    },
+-- 3. Enable standard servers
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
 end
 
--- 2. Setup with "Unknown Scheme" middleware
-lspconfig.shopware_lsp.setup {
+-- 4. Configure Intelephense specifically to fix the "Undefined type" errors
+lspconfig.intelephense.setup {
   on_attach = nvlsp.on_attach,
   capabilities = nvlsp.capabilities,
-  handlers = {
-    -- Fixes "Go to Definition" and "References"
-    ["textDocument/definition"] = function(err, result, ctx, config)
-      if result then
-        if vim.islist(result) then
-          for _, res in ipairs(result) do res.uri = fix_uri(res.uri) end
-        else
-          result.uri = fix_uri(result.uri)
-        end
-      end
-      vim.lsp.handlers["textDocument/definition"](err, result, ctx, config)
-    end,
-    -- Fixes "Unknown Scheme" error for Diagnostics/Errors
-    ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-      if result and result.uri then
-        result.uri = fix_uri(result.uri)
-      end
-      vim.lsp.handlers["textDocument/publishDiagnostics"](err, result, ctx, config)
-    end,
-  },
+  -- FORCE the root to be the main shopware folder, not the plugin folder
+  root_dir = function(fname)
+    return util.root_pattern("bin/console", "composer.lock", ".git")(fname) 
+           or util.root_pattern("composer.json")(fname)
+  end,
+  settings = {
+    intelephense = {
+      files = {
+        maxSize = 5000000; -- Increase file size limit if needed
+      },
+      environment = {
+        includePaths = {
+          -- Explicitly tell it where the vendor folder is relative to your plugin
+          -- This helps if it still gets confused
+          "vendor/**", 
+          "../../vendor/**" 
+        }
+      }
+    }
+  }
 }
