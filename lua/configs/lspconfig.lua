199220984342ee9f359db45d5d89de7e816958a7
gazi04 <gazmendhalili2016@gmail.com>
2026-02-07 12:52:45 +0100

    :bug: Solved shadow lsp that the tailwindcss lsp caused

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,15 +1,41 @@
 local lsp_util = require("lspconfig.util")
 
-local servers = {
-  "pyright",
-  "ts_ls",
-  "intelephense",
-  "tailwindcss",
-  "html",
-  "cssls",
-  "svelte"
-}
+local servers = { "pyright", "ts_ls", "html", "cssls" }
 vim.lsp.enable(servers)
+
+vim.lsp.config['svelte'] = {
+  cmd = { "svelteserver", "--stdio" },
+  filetypes = { "svelte" },
+  root_dir = function(fname)
+    return lsp_util.root_pattern("package.json", ".git")(fname)
+  end,
+  -- This tells Svelte how to find your TypeScript files
+  settings = {
+    svelte = {
+      plugin = {
+        svelte = { enabled = true },
+        typescript = { enabled = true },
+        css = { enabled = true },
+      }
+    }
+  }
+}
+vim.lsp.enable("svelte")
+
+vim.lsp.config['tailwindcss'] = {
+  cmd = { "tailwindcss-language-server", "--stdio" },
+  -- Explicitly include svelte, but we'll use a hack to ensure it doesn't block
+  filetypes = { "html", "css", "svelte", "javascriptreact", "typescriptreact" },
+  root_dir = lsp_util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js'),
+  settings = {
+    tailwindCSS = {
+      includeLanguages = {
+        svelte = "html", -- Treat the markup part of svelte as html for tailwind
+      },
+    },
+  },
+}
+vim.lsp.enable("tailwindcss")
 
 vim.lsp.config['intelephense'] = {
   cmd = { "intelephense", "--stdio" },
@@ -34,13 +60,3 @@ vim.lsp.config['intelephense'] = {
 }
 
 vim.lsp.enable("intelephense")
-
-vim.lsp.config['svelte'] = {
-  cmd = { "svelteserver", "--stdio" },
-  filetypes = { "svelte" },
-  root_dir = function(fname)
-    return lsp_util.root_pattern("package.json", ".git")(fname)
-  end,
-}
-
-vim.lsp.enable("svelte")
