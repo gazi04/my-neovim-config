e0f9e3a4865c2b747020a3f49464b1c4b6e31576
gazi04 <gazmendhalili2016@gmail.com>
2026-02-12 21:28:43 +0100

    :bug: Transitioning lsp configs to 0.11 native API keeping shopware root detection for intelephense and resolving svelte, tailwind shadowing conflicts

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,62 +1,65 @@
 local lsp_util = require("lspconfig.util")
+local configs = require("lspconfig.configs")
 
-local servers = { "pyright", "ts_ls", "html", "cssls" }
-vim.lsp.enable(servers)
+-- 1. Setup Standard Servers
+vim.lsp.enable({ "pyright", "ts_ls", "html", "cssls" })
 
-vim.lsp.config['svelte'] = {
-  cmd = { "svelteserver", "--stdio" },
-  filetypes = { "svelte" },
-  root_dir = function(fname)
-    return lsp_util.root_pattern("package.json", ".git")(fname)
-  end,
-  -- This tells Svelte how to find your TypeScript files
-  settings = {
-    svelte = {
-      plugin = {
-        svelte = { enabled = true },
-        typescript = { enabled = true },
-        css = { enabled = true },
-      }
-    }
-  }
-}
-vim.lsp.enable("svelte")
-
-vim.lsp.config['tailwindcss'] = {
-  cmd = { "tailwindcss-language-server", "--stdio" },
-  -- Explicitly include svelte, but we'll use a hack to ensure it doesn't block
-  filetypes = { "html", "css", "svelte", "javascriptreact", "typescriptreact" },
-  root_dir = lsp_util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js'),
-  settings = {
-    tailwindCSS = {
-      includeLanguages = {
-        svelte = "html", -- Treat the markup part of svelte as html for tailwind
-      },
-    },
-  },
-}
-vim.lsp.enable("tailwindcss")
-
-vim.lsp.config['intelephense'] = {
-  cmd = { "intelephense", "--stdio" },
-  filetypes = { "php" },
-  root_dir = function(fname)
-    -- Look for Shopware markers first, fallback to composer.json
-    local root = lsp_util.root_pattern("bin/console", "composer.lock", ".git")(fname)
-               or lsp_util.root_pattern("composer.json")(fname)
-    return root
-  end,
-  settings = {
-    intelephense = {
-      files = { maxSize = 5000000 },
-      environment = {
-        includePaths = {
-          "vendor/**", 
-          "../../vendor/**" 
+-- 2. Intelephense (Fixed for 0.11)
+if not configs.intelephense then
+  configs.intelephense = {
+    default_config = {
+      cmd = { "intelephense", "--stdio" },
+      filetypes = { "php" },
+      root_dir = function(fname)
+        return lsp_util.root_pattern("bin/console", "composer.json", ".git")(fname) 
+               or vim.uv.cwd()
+      end,
+      settings = {
+        intelephense = {
+          files = { maxSize = 5000000 },
+          environment = {
+            includePaths = { "vendor/**", "../../vendor/**" }
+          }
         }
       }
     }
   }
-}
-
+end
 vim.lsp.enable("intelephense")
+
+-- 3. Svelte (Fixed for 0.11)
+if not configs.svelte then
+  configs.svelte = {
+    default_config = {
+      cmd = { "svelteserver", "--stdio" },
+      filetypes = { "svelte" },
+      root_dir = lsp_util.root_pattern("package.json", ".git"),
+      settings = {
+        svelte = {
+          plugin = {
+            typescript = { enabled = true },
+            css = { enabled = true },
+          }
+        }
+      }
+    }
+  }
+end
+vim.lsp.enable("svelte")
+
+-- 4. Tailwind (Fixed for 0.11)
+if not configs.tailwindcss then
+  configs.tailwindcss = {
+    default_config = {
+      cmd = { "tailwindcss-language-server", "--stdio" },
+      filetypes = { "html", "css", "svelte", "javascriptreact", "typescriptreact" },
+      root_dir = lsp_util.root_pattern('tailwind.config.js', 'postcss.config.js'),
+      settings = {
+        tailwindCSS = {
+          includeLanguages = { svelte = "html" },
+        },
+      }
+    }
+  }
+end
+vim.lsp.enable("tailwindcss")
