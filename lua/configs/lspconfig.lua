a9c09eb60ad10bf39edbf70530f0e5c54f87a684
gazi04 <gazmendhalili2016@gmail.com>
2026-02-12 21:36:38 +0100

    :bug: Going back from the vim.lsp API to the old lspconfig.setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,65 +1,57 @@
-local lsp_util = require("lspconfig.util")
-local configs = require("lspconfig.configs")
+local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
 
--- 1. Setup Standard Servers
-vim.lsp.enable({ "pyright", "ts_ls", "html", "cssls" })
+-- This function is standard for NvChad to map keys only when LSP attaches
+local on_attach = function(client, bufnr)
+  -- NvChad specific: if you have a custom on_attach in your main config, 
+  -- you might need to call it here.
+end
 
--- 2. Intelephense (Fixed for 0.11)
-if not configs.intelephense then
-  configs.intelephense = {
-    default_config = {
-      cmd = { "intelephense", "--stdio" },
-      filetypes = { "php" },
-      root_dir = function(fname)
-        return lsp_util.root_pattern("bin/console", "composer.json", ".git")(fname) 
-               or vim.uv.cwd()
-      end,
-      settings = {
-        intelephense = {
-          files = { maxSize = 5000000 },
-          environment = {
-            includePaths = { "vendor/**", "../../vendor/**" }
-          }
-        }
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+-- Standard servers list
+local servers = { "pyright", "ts_ls", "html", "cssls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- 1. Svelte Setup
+lspconfig.svelte.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "svelte" },
+  root_dir = util.root_pattern("package.json", ".git"),
+}
+
+-- 2. Tailwind Setup
+lspconfig.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "css", "svelte", "javascriptreact", "typescriptreact" },
+  root_dir = util.root_pattern('tailwind.config.js', 'postcss.config.js', '.git'),
+}
+
+-- 3. Intelephense Setup (The most important one right now)
+lspconfig.intelephense.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "intelephense", "--stdio" },
+  filetypes = { "php" },
+  -- This force-returns the current directory if it can't find a root, 
+  -- just to ensure it STARTS no matter what.
+  root_dir = function(fname)
+    return util.root_pattern("bin/console", "composer.json", ".git")(fname) or vim.loop.cwd()
+  end,
+  settings = {
+    intelephense = {
+      files = { maxSize = 5000000 },
+      environment = {
+        includePaths = { "vendor/**", "../../vendor/**" }
       }
     }
   }
-end
-vim.lsp.enable("intelephense")
-
--- 3. Svelte (Fixed for 0.11)
-if not configs.svelte then
-  configs.svelte = {
-    default_config = {
-      cmd = { "svelteserver", "--stdio" },
-      filetypes = { "svelte" },
-      root_dir = lsp_util.root_pattern("package.json", ".git"),
-      settings = {
-        svelte = {
-          plugin = {
-            typescript = { enabled = true },
-            css = { enabled = true },
-          }
-        }
-      }
-    }
-  }
-end
-vim.lsp.enable("svelte")
-
--- 4. Tailwind (Fixed for 0.11)
-if not configs.tailwindcss then
-  configs.tailwindcss = {
-    default_config = {
-      cmd = { "tailwindcss-language-server", "--stdio" },
-      filetypes = { "html", "css", "svelte", "javascriptreact", "typescriptreact" },
-      root_dir = lsp_util.root_pattern('tailwind.config.js', 'postcss.config.js'),
-      settings = {
-        tailwindCSS = {
-          includeLanguages = { svelte = "html" },
-        },
-      }
-    }
-  }
-end
-vim.lsp.enable("tailwindcss")
+}
